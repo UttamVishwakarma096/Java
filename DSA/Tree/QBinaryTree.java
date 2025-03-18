976ecc06acc232b27d01bdb5c085c454e513656c
UttamVishwakarma096 <uttamvishwakarma096@gmail.com>
2025-03-18 17:18:35 +0530

    update

--- DSA/Tree/QBinaryTree.java
+++ DSA/Tree/QBinaryTree.java
@@ -27,17 +27,48 @@ public class QBinaryTree {
         return newNode;
     }
 
-    public int countNodes(Node root) {
+    public int countOfNodes(Node root) {
         if(root == null) {
             return 0;
         }
-        return countNodes(root.left) + countNodes(root.right) + 1;
+        return countOfNodes(root.left) + countOfNodes(root.right) + 1;
+    }
+
+    public int sumOfNodes(Node root) {
+        if(root == null) {
+            return 0;
+        }
+        return sumOfNodes(root.left) + sumOfNodes(root.right) + root.data;
+    }
+
+    public int height(Node root) {
+        if(root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        int myHeight = Math.max(leftHeight, rightHeight) + 1;
+        return myHeight;
+    }
+
+    public int diameter(Node root) {
+        if(root == null) {
+            return 0;
+        }
+        int diam1 = diameter(root.left);
+        int diam2 = diameter(root.right);
+        int diam3 = height(root.left) + height(root.right) + 1;
+        return Math.max(diam3, Math.max(diam1, diam2));
     }
     public static void main(String[] args) {
         int[] node = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         QBinaryTree tree = new QBinaryTree();
         Node root = tree.BuildTree(node);
 
-        System.out.println("Total no of Nodes : "+tree.countNodes(root));
+        System.out.println("Total no of Nodes : "+tree.countOfNodes(root));
+        System.out.println("Sum of Nodes value : "+tree.sumOfNodes(root));
+        System.out.println("Height of Tree : "+tree.height(root));
+        System.out.println("Diameter of Tree : "+tree.diameter(root));
     }
 }
