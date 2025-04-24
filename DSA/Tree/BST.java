9ab1834ef0c1d0c517fe4cf82b4c4d578fe5d2b8
UttamVishwakarma096 <uttamvishwakarma096@gmail.com>
2025-04-24 14:07:25 +0530

    deleting Node

--- DSA/Tree/BST.java
+++ DSA/Tree/BST.java
@@ -53,6 +53,33 @@ public class BST {
             return search(root.right, key);
         }
     }
+
+    public Node delete(Node root, int val) {
+        if(root.data > val) {
+            root.left = delete(root.left, val);
+        } else if(root.data < val) {
+            root.right = delete(root.right, val);
+        } else {
+            if(root.left == null && root.right == null) {
+                return null;
+            } else if(root.left == null) {
+                return root.right;
+            } else if(root.right == null) {
+                return root.left;
+            }
+            Node IS = inorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+
+    private Node inorderSuccessor(Node root) {
+        while(root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
     public static void main(String[] args) {
         int[] values = {8,5,3,1,4,6,10,11,14};
         BST tree = new BST();
@@ -61,10 +88,8 @@ public class BST {
         tree.inOrder(root);
         System.out.println();
 
-        if(tree.search(root, 7)) {
-            System.out.println("Found");
-        } else {
-            System.out.println("Not found");
-        }
+        tree.delete(root, 10);
+
+        tree.inOrder(root);
     }    
 }
